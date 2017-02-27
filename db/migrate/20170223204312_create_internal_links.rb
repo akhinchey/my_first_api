3e1ff75d50ab576a62b974b08cbbd62debe8426d
Amanda Hinchey <akhinchey@gmail.com>
2017-02-26 20:06:37 -0500

    Add link names in db.

--- db/migrate/20170223204312_create_internal_links.rb
+++ db/migrate/20170223204312_create_internal_links.rb
@@ -2,7 +2,8 @@ class CreateInternalLinks < ActiveRecord::Migration[5.0]
   def change
     create_table :internal_links do |t|
       t.references :url, index: true
-      t.string :content
+      t.string :name, null: false
+      t.string :content, null: false
       t.timestamps
     end
   end
