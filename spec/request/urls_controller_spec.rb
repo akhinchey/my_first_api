3e23dae35814b6487605e5fc289dbffb880f9e7d
Amanda Hinchey <akhinchey@gmail.com>
2017-02-26 22:29:29 -0500

    Clean up code styles.

--- spec/request/urls_controller_spec.rb
+++ spec/request/urls_controller_spec.rb
@@ -1,27 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "UrlsController", :type => :request do
+RSpec.describe 'UrlsController', :type => :request do
 
-  describe "GET /urls" do
-    it "gets all currently stored urls in JSON format" do
+  describe 'GET /urls' do
+    it 'gets all currently stored urls in JSON format' do
       headers = {
-        "ACCEPT" => "application/json"
+        'ACCEPT' => 'application/json'
       }
-      get "http://localhost:3000/api/v1/urls", headers: headers
+      get 'http://localhost:3000/api/v1/urls', headers: headers
 
-      expect(response.content_type).to eq("application/json")
+      expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe "POST /urls" do
-    it "stores valid urls in JSON format" do
+  describe 'POST /urls' do
+    it 'stores valid urls in JSON format' do
       headers = {
-          "ACCEPT" => "application/json"
+          'ACCEPT' => 'application/json'
       }
-      post "http://localhost:3000/api/v1/urls", params: { :url => {:name => "https://www.nytimes.com/"} }, headers:headers
+      post 'http://localhost:3000/api/v1/urls', params: { :url => {:name => 'https://www.nytimes.com/'} }, headers:headers
 
-      expect(response.content_type).to eq("application/json")
+      expect(response.content_type).to eq('application/json')
       expect(response).to have_http_status(:ok)
     end
   end
